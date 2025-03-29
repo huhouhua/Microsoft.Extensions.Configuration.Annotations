dcd5e8e1370fb9956d218511f3db732245f557fa
huhouhua <huhouhuam@outlook.com>
2025-03-29 19:21:51 +0800

    test: remove Useless test class

--- test/Microsoft.Extensions.Configuration.Annotations.Test/Utilities/AnnotationFixture.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Kevin Berger Authors. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-namespace Microsoft.Extensions.Configuration.Annotations.Test.Utilities;
-
-public class AnnotationFixture
-{
-    
-}
