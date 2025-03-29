4e56f55f60c96383125d9ad5110fde6a4c0509f6
huhouhua <huhouhuam@outlook.com>
2025-03-29 19:18:15 +0800

    feat: add webapi project use examples

---
+++ examples/Annotations.WebAPI.Examples/AppOptions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration.Annotations;
+using Microsoft.Extensions.Options;
+
+namespace Annotations.WebAPI.Examples;
+
+[Validate(typeof(AppValidateOptions))]
+[Options("app")]
+public class AppOptions
+{
+    public int Id { get; set; }
+    
+    public string Name {get; set; }
+    
+    public string Version { get; set; }
+    
+    public string Description { get; set; }
+}
+
+
+public class AppValidateOptions : IValidateOptions<AppOptions>
+{
+    public ValidateOptionsResult Validate(string name, AppOptions options)
+    {
+        // To do validate for AppOptions
+        
+        return ValidateOptionsResult.Success;
+    }
+}
---
+++ examples/Annotations.WebAPI.Examples/Program.cs
@@ -0,0 +1,57 @@
+using Annotations.WebAPI.Examples;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Injection of Microsoft.Extensions.Configuration.Annotations components
+builder.Services.AddAttributeConfigurationOptions(builder.Configuration,false , typeof(Program).Assembly);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+// Use IOptions to get AppOptions
+app.MapGet("/app", (IOptions<AppOptions> options) =>
+{
+    return options.Value;
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
